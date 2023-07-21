7621475d2f0a7df78a4a743e5d7d28111e81b888
mingchen@ahaent.com <mingchen@ahaent.com>
2023-07-21 11:47:16 +0800

    maint: add some example for js

---
+++ array.js
@@ -0,0 +1,21 @@
+var cars = new Array();
+
+cars.push('little boy')
+
+console.log(cars)
+
+cars.unshift('beautiful girl')
+
+console.log(cars)
+
+cars.reverse()
+
+console.log(cars)
+
+cars.sort()
+
+console.log(cars)
+
+cars.sort()
+
+console.log(cars)
---
+++ map.js
@@ -0,0 +1,8 @@
+const a = {
+    user: ['Jimi','Bob'],
+    age:19,
+    coclor: 'blue',
+};
+
+console.log(a);
+console.log(a.coclor);
